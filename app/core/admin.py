743515d4a59d0e8b2c4d7458f27c2d17a9f1de25
Disha Arora <dishaarora1996@gmail.com>
2023-09-19 06:20:54 +0530

    updated tests

--- app/core/admin.py
+++ app/core/admin.py
@@ -1,3 +1,52 @@
 from django.contrib import admin # noqa
+from django.utils.translation import gettext_lazy as _
 
 # Register your models here.
+
+
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+
+from core.models import *
+
+class UserAdmin(BaseUserAdmin):
+    ordering=['id']
+    list_display =['email', 'name']
+    fieldsets = (
+        (None, {'fields': ('email', 'password')}),
+        (_('Personal Info'), {'fields': ('name',)}),
+        (
+            _('Permissions'),
+            {
+                'fields': (
+                    'is_active',
+                    'is_staff',
+                    'is_superuser',
+                )
+            }
+        ),
+        (_('Important dates'), {'fields': ('last_login',)}),
+    )
+    readonly_fields = ['last_login']
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': (
+                'email',
+                'password1',
+                'password2',
+                'name',
+                'is_active',
+                'is_staff',
+                'is_superuser',
+            ),
+        }),
+    )
+
+
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Recipe)
+admin.site.register(Tag)
+admin.site.register(Ingredient)
+
+
